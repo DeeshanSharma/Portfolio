1ec4aa1f8c017bb030b06a5b1cd4589f30ddc9c1
Deeshan Sharma <deeshansharma04@gmail.com>
2020-12-31 17:45:23 +0530

    chore: Renamed file

---
+++ js/color-def.js
@@ -0,0 +1,22 @@
+var sections = document.getElementsByTagName("section");
+var buttons = document.querySelectorAll(".section .link");
+var sectionClasses = ["", " pink-colored", " purple-section", " purple-colored"];
+var buttonClasses = [" primary-link", " secondary-link"];
+var url = window.location.pathname;
+
+var i = 0;
+var j = 0;
+for (var k = 0; k < sections.length; k++) {
+	sections[k].className += sectionClasses[i];
+	i += 1;
+	if (i > 3) {
+		i = 0;
+	}
+	if (url === "/blogs.html") {
+		buttons[k].className += buttonClasses[j];
+		j += 1;
+		if (j > 1) {
+			j = 0;
+		}
+	}
+}
